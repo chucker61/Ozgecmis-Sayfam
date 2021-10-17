3ed5542505c3c1b4fc463ba5376322905df666dd
Melikşah Mert Çakır <80900478+chucker61@users.noreply.github.com>
2021-10-17 22:42:18 +0300

    Add files via upload

---
+++ jsDosyasi.js
@@ -0,0 +1,7 @@
+function yakala(adsoyad, mail, konu, mesaj){
+
+      document.getElementById("tdAdSoyad").innerHTML = adsoyad;
+      document.getElementById("tdMail").innerHTML = mail;
+      document.getElementById("tdKonu").innerHTML = konu;
+      document.getElementById("tdMesaj").innerHTML = mesaj;
+    }
